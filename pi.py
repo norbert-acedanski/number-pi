bb280afc5e8ac731dcb4aaa06f9e5bd81f71d355
Norbert Acedanski <norbert.acedanski@gmail.com>
2022-03-14 20:33:39 +0100

    Add Ramanujan's formula

--- pi.py
+++ pi.py
@@ -1,3 +1,4 @@
+from cmath import sqrt
 import math
 
 class calculatePi():
@@ -21,7 +22,7 @@ class calculatePi():
             print("The first few elements: 1/1 - 1/3 + 1/5 - 1/7 + 1/9 - ... = π/4")
             print("It is one of the methods, that takes a very large number of the \"n\" to see the convergence")
         pi = 0
-        for i in range(numberOfElements):
+        for i in range(numberOfElements + 1):
             pi += (-1)**i/(2*i + 1)
         pi *= 4
         print("\nπ calculated with Leibniz formula, using " + str(numberOfElements) + " elements (n) is equal to: " + str(pi))
@@ -43,7 +44,6 @@ class calculatePi():
         if printBasicInfo:
             print("\nNewton formula states, that the sum from n=0 to infinity of 2^n*(k!)^2/(2n + 1)! = π/2")
             print("The first few elements: 1 + 1/3(1 + 2/5(1 + 3/7(1 + ...))) = π/2")
-            # print("It is one of the methods, that takes a very large number of the \"n\" to see the convergence")
         pi = 0
         for i in range(0, numberOfElements + 1):
             pi += (2**i)*math.factorial(i)**2/math.factorial(2*i + 1)
@@ -64,7 +64,17 @@ class calculatePi():
         return pi
     
     def ramanujanFormula(self, numberOfElements: int, printBasicInfo: bool=True):
-        print("")
+        if printBasicInfo:
+            print("\nRamanujan's formula states, that the product of the 2*sqrt(2)/9801 and the sum from n=0 to infinity of (4n)!(1103 + 26390n)/[(n!)^4*396^(4n)]= 1/π")
+            # print("The first few elements: 1/(2*3*4) - 1/(4*5*6) + 1/(6*7*8) - ... = 1/π")
+            # print("It is one of the methods, that takes a very large number of the \"n\" to see the convergence")
+        pi = 0
+        for i in range(0, numberOfElements + 1):
+            pi += math.factorial(4*i)*(1103 + 26390*i)/(math.factorial(i)**4*396**(4*i))
+        pi *= 2*math.sqrt(2)/9801
+        pi = 1/pi
+        print("\nπ calculated with Ramanujan's formula, using " + str(numberOfElements) + " elements (n) is equal to: " + str(pi))
+        return pi
     
     def chudnovskyFormula(self, numberOfElements: int, printBasicInfo: bool=True):
         print("")
@@ -74,5 +84,6 @@ if __name__ == "__main__":
     piApproximations.printBasicInfoAboutPi()
     piApproximations.leibnizFormula(10)
     piApproximations.johnWallisFormula(10)
+    piApproximations.newtonFormula(10)
     piApproximations.nilakanthaFormula(10)
-    piApproximations.newtonEulerFormula(10)
+    piApproximations.ramanujanFormula(1)
